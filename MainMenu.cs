895bee96d2816889505d626ce6ad0a2f9aa03641
JHT1609 <johannesto245@nemi.tskoli.is>
2019-05-13 16:51:05 +0000

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //fer yfir á næsta scene í build hierarchy 
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT");  //Loggar QUIT
+        Application.Quit(); //Lokar Applicationinu
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;    //Setur CharacterContoller2D script sem controller
+
+    public float runSpeed = 40f;    //Setur Default Runspeed sem 40f
+
+    float horizontalMove = 0f;
+
+    bool jump = false;      //passar að character er ekki stanslaust að hoppa eða skríða
+    bool crouch = false;
+
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; //Movement speed er áttin sem er að fara * runspeed floatið
+
+        if (Input.GetButtonDown("Jump"))    //ef jump takkin er ýttur, á player að stökkva
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+
+        else if (Input.GetButtonUp("Crouch"))   //ef ekki er verið að ýta á crouch takka er crouch aftur false
+        {
+            crouch = false;
+        }
+
+    }
+
+    void FixedUpdate ()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);    //Player Hoppar í áttina sem hann er að horfa og kíkir svo hvort player vill aftur hoppa eða skríða
+        jump = false;
+    }
+}
